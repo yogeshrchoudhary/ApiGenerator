28d3c799b0626e1a044a26e250b2b50855ec946c
Choudhary, Yogesh <Yogesh.Choudhary@capita.co.uk>
2020-01-23 17:09:34 +0530

    Add project files.

---
+++ ApiGenerator/autogen/ControllerGenerator.cs
@@ -0,0 +1,18 @@
+﻿//This is an auto-generated file and shouldn't be checked-in
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGenerator.Controllers
+{
+
+	[ApiController]
+	[Route("api/v1/Country")]
+	public partial class CountryController: ControllerBase
+	{
+    }
+	[ApiController]
+	[Route("api/v1/City")]
+	public partial class CityController: ControllerBase
+	{
+    }
+
+}
