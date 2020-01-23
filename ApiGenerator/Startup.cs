9063ece4408a899efc51f0f5a2263b78d5076c2c
Choudhary, Yogesh <Yogesh.Choudhary@capita.co.uk>
2020-01-23 20:12:29 +0530

    Config generated controllers

--- ApiGenerator/Startup.cs
+++ ApiGenerator/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ApiGenerator
 {
@@ -26,6 +27,13 @@ namespace ApiGenerator
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Generated API",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +53,11 @@ namespace ApiGenerator
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Generated API");
             });
         }
     }
